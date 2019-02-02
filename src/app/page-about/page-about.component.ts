c6e10ea9fe3596ec12aa388e45aaaf0352e62d21
Miles Exner <dizolivemint@gmail.com>
2019-02-02 09:06:34 -0800

    Add meta tags

--- src/app/page-about/page-about.component.ts
+++ src/app/page-about/page-about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-page-about',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageAboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(meta: Meta, title: Title) {
+    title.setTitle('Amelia Exner | Marriage and Family Therapy | About');
+    
+    meta.addTags([
+      { name: 'author', content: 'Amelia Exner' },
+      { name: 'keywords', content: 'San Diego, MFT, Marriage, Family, Counselor, Therapy, Therapist' },
+      { name: 'description', content: 'About Amelia Exner, Marriage and Family Therapist, practicing therapy in the San Diego, Califonia region.'}
+    ]);
+  }
+   
 
   ngOnInit() {
   }
